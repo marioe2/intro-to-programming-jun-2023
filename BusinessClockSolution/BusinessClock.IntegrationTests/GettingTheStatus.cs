1494139e0315d8a54c59829a115e1988f4fab788
Mario Esquivel <marioesquivel43@gmail.com>
2023-06-19 16:45:13 -0400

    updated fix for tests

--- BusinessClockSolution/BusinessClock.IntegrationTests/GettingTheStatus.cs
+++ BusinessClockSolution/BusinessClock.IntegrationTests/GettingTheStatus.cs
@@ -22,5 +22,23 @@ namespace BusinessClock.IntegrationTests
             Assert.True(responseMessage.Open);
             Assert.Null(responseMessage.OpensAt);
         }
+
+        [Fact]
+        public async Task ClosedHours()
+        {
+            var host = await AlbaHost.For<Program>();
+
+            var response = await host.Scenario(api =>
+            {
+                api.Get.Url("/status");
+                api.StatusCodeShouldBeOk();
+            });
+
+            Assert.NotNull(response);
+            GetStatusResponse? responseMessage = response.ReadAsJson<GetStatusResponse>();
+            Assert.NotNull(responseMessage);
+            Assert.False(responseMessage.Open);
+            Assert.NotNull(responseMessage.OpensAt);
+        }
     }
 }
