3b8617f0d2968e55c4ee23f9073fc73a3d10148d
Mario Esquivel <marioesquivel43@gmail.com>
2023-06-23 15:03:19 -0400

    my attempt

---
+++ GreeterPractice/Greeter.UnitTests/GreetingTests.cs
@@ -0,0 +1,88 @@
+﻿using Castle.Core.Logging;
+using Moq;
+
+namespace Greeter.UnitTests;
+public  class GreetingTests
+{
+    private readonly Mock<IBlackList> _blackList;
+    private readonly Mock<INotification> _notification;
+    private readonly GreetingMaker _greetingMaker;
+
+    public GreetingTests()
+    {
+        this._blackList = new Mock<IBlackList>();
+        this._notification = new Mock<INotification>();
+        this._greetingMaker = new GreetingMaker(new Mock<IBlackList>().Object, new Mock<INotification>().Object);
+    }
+
+    [Theory]
+    [InlineData("Windom", "Hello, Windom.")]
+    public void SingleName(string name, string expected)
+    {
+        string greeting = _greetingMaker.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("", "Hello, Chief!")]
+    public void NullName(string name, string expected)
+    { 
+        string greeting = _greetingMaker.Greet(null);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("WINDOM", "HELLO, WINDOM!")]
+    public void UppercaseName(string name, string expected)
+    {
+        string greeting = _greetingMaker.Greet(name);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Theory]
+    [InlineData("Bob", "Sue", "Hello, Bob and Sue!")]
+    public void TwoNames(string name1, string name2, string expected)
+    {
+        string greeting = _greetingMaker.Greet(name1, name2);
+
+        Assert.Equal(expected, greeting);
+    }
+
+    [Fact]
+    public void MultipleNames()
+    {
+        string greeting = _greetingMaker.Greet("Bob", "Sue", "Jeff");
+
+        Assert.Equal("Hello, Bob, Sue, and Jeff!", greeting);
+    }
+
+    [Fact]
+    public void MultipleNamesUpper()
+    {
+        string greeting = _greetingMaker.Greet("BOB", "SUE", "JEFF");
+
+        Assert.Equal("HELLO, BOB, SUE, AND JEFF!", greeting);
+    }
+
+    [Fact]
+    public void MultipleNamesMixed()
+    {
+        string greeting = _greetingMaker.Greet("BOB", "Sue", "Jeff");
+
+        Assert.Equal("Hello, Sue, Jeff, AND BOB!", greeting);
+    }
+
+    [Fact]
+    public void BlacklistedNames()
+    {
+        string greeting = _greetingMaker.Greet("Bob", "Jack", "Jeff");
+
+        _blackList.Setup(m => m.Check("Jack")).Throws(new Exception());
+
+        Assert.Equal("Hello, Sue, Jeff, AND BOB!", greeting);
+    }
+}
+
---
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -0,0 +1,124 @@
+﻿using System.Xml.Linq;
+
+namespace Greeter
+{
+    public class GreetingMaker
+    {
+        public IBlackList _blackList;
+        public INotification _notification;
+
+        public GreetingMaker(IBlackList blackList, INotification notification)
+        {
+            _blackList = blackList;
+            _notification = notification;  
+        }
+
+        /*        public string Greet(string? name, params string[] rest)
+                {
+                    if (name == null)
+                    {
+                        return "Hello, Chief!";
+                    }
+
+                    if (IsAllUpper(name))
+                    {
+                        return $"Hello, {name}!".ToUpper();
+                    }
+
+                    return $"Hello, {name}.";
+                }*/
+
+        public string Greet(params string[]? names)
+        {
+            var greeting = "Hello";
+            var nameString = "";
+
+            if (names == null)
+            {
+                return "Hello, Chief!";
+            }
+
+            if (names.Length == 1)
+            {
+                if (IsAllUpper(names[0]))
+                {
+                    return $"Hello, {names[0]}!".ToUpper();
+                }
+                else
+                {
+                    return $"Hello, {names[0]}.";
+                }
+            }
+
+            if (names.Length == 2)
+            {
+                foreach (string name in names)
+                {
+                    if (IsAllUpper(name))
+                    {
+                        greeting += $", {name}".ToUpper();
+                    }
+                    else if (name == names.Last())
+                    {
+                        greeting += $" and {name}";
+                    }
+                    else
+                    {
+                        greeting += $", {name}";
+                    }
+                }
+            }
+
+            else
+            {
+                foreach (string name in names)
+                {
+                    if (!IsAllUpper(name) && name == names.Last())
+                    {
+                        nameString += $", and {name}";
+                    }
+                    else if (IsAllUpper(name) && name == names.Last())
+                    {
+                        nameString += $", AND {name}";
+                    }
+                    else if (IsAllUpper(name))
+                    {
+                        nameString += $", {name}";
+                    }
+                    else
+                    {
+                        nameString += $", {name}";
+                    }
+                }
+
+                if (IsAllUpper(nameString))
+                {
+                    return greeting.ToUpper() + nameString + "!";
+                }
+            }
+
+            return greeting + nameString + "!";
+            
+        }
+
+        public bool IsAllUpper(string input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+
+    public interface INotification
+    {
+    }
+
+    public interface IBlackList
+    {
+        object Check(string name);
+    }
+}
+
